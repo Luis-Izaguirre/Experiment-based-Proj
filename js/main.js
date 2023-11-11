60189ed45500495c8c6b8d9c15d2df59604b0586
Luis-Izaguirre <lmunozi181i@gmail.com>
2023-11-10 20:05:41 -0800

    Updated main.js, working on the add cart functionality!!! Don't forget to add comments for later, that way you don't get lossed.

--- js/main.js
+++ js/main.js
@@ -30,10 +30,65 @@ function ready() {
         var button = removeCartButtons[i];
         button.addEventListener("click", removeCartItem);
     }
+    //Quantity Changes,
+    //Loops through and checks all "cart-quantity" for an event
+    var quantityInputs = document.getElementsByClassName("cart-quantity");
+    for (var i = 0; i < quantityInputs.length; i++){
+        var input = quantityInputs[i];
+        input.addEventListener("change", quantityChanged);
+    }
+    //Add To Cart
+    var addCart = document.getElementsByClassName("add-cart");
+    for (var i = 0; i < addCart.length; i++){
+        var button = addCart[i];
+        button.addEventListener("click", addCartClicked);
+    }
 }
 
 // Remove Items From Cart, function to remove an item
 function removeCartItem(event){
     var buttonClicked = event.target;
     buttonClicked.parentElement.remove();
+    updateTotal();
+}
+// Quantity Changes
+function quantityChanged(event){
+    var input = event.target;
+    if(isNaN(input.value) || input.value <= 0){
+        input.value = 1;
+    }
+    updateTotal();
+}
+
+// Add To Cart
+function addCartClicked(event){
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
+    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    //FIX ME, (48:04) ALSO ADD COMMENTS TO THE UPDATED CODE!!!!
+    console.log(title, price, productImg);
+}
+
+
+// Update Total, from parent element get children class and get individual "cartBox" classes
+//Loop iterates through individual "cartBox", gets it's price, quantity, and removes "$"
+// Does simple math and adds the total to document
+
+function updateTotal(){
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    var cartBoxes = cartContent.getElementsByClassName("cart-box");
+    var total = 0;
+
+    for (var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+        var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+        var price = parseFloat(priceElement.innerText.replace('$', ""));
+        var quantity = quantityElement.value;
+        total = total + (price * quantity);
+
+        document.getElementsByClassName("total-price")[0].innerText = '$' + total;
+    }
 }
