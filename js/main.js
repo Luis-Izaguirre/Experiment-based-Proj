a59b4637f169658ee8836a90301232a076510f86
Luis-Izaguirre <lmunozi181i@gmail.com>
2023-11-10 17:56:34 -0800

    New changes to interface, layout and JS!!!

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Cart
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+// Open Cart
+cartIcon.onclick = () => {
+    cart.classList.add("active");
+};
+
+// Close Cart
+closeCart.onclick = () => {
+    cart.classList.remove("active");
+};
+
+
+// Cart Working JS
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", ready)
+}else{
+   ready(); 
+}
+
+// Making Function
+function ready() {
+    // Remove Items From Cart
+    var removeCartButtons = document.getElementsByClassName("cart-remove");
+    console.log(removeCartButtons);
+    for (var i = 0; i < removeCartButtons.length; i++) {
+        var button = removeCartButtons[i];
+        button.addEventListener("click", removeCartItem);
+    }
+}
+
+// Remove Items From Cart
+function removeCartItem(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+}
