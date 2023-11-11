995e858f006262c704c6a0d868192909c5b083ca
Luis-Izaguirre <lmunozi181i@gmail.com>
2023-11-10 22:45:15 -0800

    Added more content, making the template dynamic Worked more on functionality in javascript and made a peice of html modular

--- js/main.js
+++ js/main.js
@@ -74,10 +74,39 @@ function addCartClicked(event){
     var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
     var price = shopProducts.getElementsByClassName("price")[0].innerText;
     var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
-    //FIX ME, (48:04) ALSO ADD COMMENTS TO THE UPDATED CODE!!!!
-    console.log(title, price, productImg);
+    addProductToCart(title, price, productImg);
+    updateTotal();
 }
 
+//FIX ME
+function addProductToCart(title, price, productImg){
+    var cartShopBox = document.createElement('div');
+    cartShopBox.classList.add('cart-box')
+    var cartItems = document.getElementsByClassName('cart-content')[0];
+    var cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
+    for(var i = 0; i < cartItemsNames.length; i++){
+        if(cartItemsNames[i].innerText == title){
+        alert('You have already add this item to cart');
+        return;
+        }
+    }
+
+var cartBoxContent = `
+                    <img src="img/product2.jpg" alt="" class="cart-img">
+                    <div class="detail-box">
+                        <div class="cart-product-title">Earbuds</div>
+                        <div class="cart-price">$25</div>
+                        <input type="number" value="1" class="cart-quantity">
+                    </div>
+                    <!-- Remove Cart -->
+                    <i class='bx bxs-trash-alt cart-remove'></i>`;
+                    
+cartShopBox.innerHTML = cartBoxContent;
+cartItems.append(cartShopBox);
+cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem);
+cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged);
+
+}// End addProductToCart Function
 
 // Update Total, from parent element get children class and get individual "cartBox" classes
 //Loop iterates through individual "cartBox", gets it's price, quantity, and removes "$"
